52fba99c54abe31a2be09fddd7be188b0d9e5ec2
Adam Mertzenich <adam+git@mertzeni.ch>
2024-03-04 19:40:29 -0600

    Cleanup and add profiles to project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
-(defproject amazon-scraper "0.1.0-SNAPSHOT"
-  :description "A work-in-progress scraping utility for Amazon search"
-  :url "https://github.com/mertzenich/"
+(defproject ch.mertzeni/swidden "0.1.1"
+  :description "A work-in-progress utility for parsing Amazon search results."
+  :url "https://github.com/Mertzenich/swidden"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.cli "1.0.219"]
-                 [cheshire "5.12.0"]
-                 [etaoin "1.0.40"]
-                 [org.clj-commons/hickory "0.7.4"]]
-  :main ^:skip-aot amazon-scraper.core
+                 [cheshire/cheshire "5.12.0"]
+                 [etaoin/etaoin "1.0.40"]
+                 [org.clj-commons/hickory "0.7.5"]
+                 [metosin/malli "0.14.0"]]
+  :main ^:skip-aot swidden.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:dependencies [[spyscope "0.1.6"]]
+                   :injections [(require 'spyscope.core)]}
+             :kaocha {:dependencies [[lambdaisland/kaocha "1.87.1366"]]}}
+  :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
